80e70031b13bf5d59cf0d8dd7152bb4e57716e08
igo21 <35313561+igo21@users.noreply.github.com>
2018-10-16 17:43:28 -0400

    atualizei alguns arquivos de imagens

--- Codigos/tela_principal/Jogo.py
+++ Codigos/tela_principal/Jogo.py
@@ -115,7 +115,7 @@ tela = Tk()
 tela['bg']="#000080"
 tela.title("G'nus Cassinos")
 tela.resizable(0, 0)
-tela.iconbitmap("imagem\logoicone3.ico")
+tela.iconbitmap("imagem\iconelogo8.ico")
 x = (tela.winfo_screenwidth() // 2) - (910 // 2)
 y = (tela.winfo_screenheight() // 2) - (600 // 2)
 tela.geometry("910x600+{}+{}".format(x, y))  # largura x altura + esquerda + topo
