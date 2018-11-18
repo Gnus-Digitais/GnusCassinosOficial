5d0090741662ed55da9eab511896243e6a11a8a4
igo21 <35313561+igo21@users.noreply.github.com>
2018-11-18 19:32:59 -0400

    setup modificado

--- Codigos/tela_principal/setup.py
+++ Codigos/tela_principal/setup.py
@@ -17,20 +17,24 @@ cx_Freeze.setup(
 '''
 import sys
 from cx_Freeze import setup, Executable
+import os
 
+os.environ['TCL_LIBRARY'] ="C:\\Users\\Igor\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tcl8.6"
+os.environ['TK_LIBRARY'] = "C:\\Users\\Igor\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tk8.6"
 
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-executables = [
-        Executable("iniciar.py", base=base,icon = "imagem/icone2.png")
+executable = [
+        Executable(script="Iniciar.py", base=base, icon="imagem/logoicone3.ico")
 ]
 
 buildOptions = dict(
-        packages = ["tkinter"],
-        includes = ["tkinter","pygame","matplotlib","time","threading","partial","os","random","sys"],
-        include_files = ["imagem/icone2.png'"],
+        packages = ["tkinter","pygame"],
+        includes = ["tkinter","pygame","matplotlib","time","threading","os","random","sys","functools"],
+        include_files = ["imagem/logoicone3.ico","Jogo.py","Howtoplay.py","imagem/aposta3.png","imagem/carteira2.png","imagem/entr_name.png","imagem/exit.png","imagem/howtoplayblackjack.png","imagem/howtoplaybutton.png","imagem/howtoplaymegastacker.png","imagem/howtoplayslotmachine.png","imagem/lb_apelido.png","imagem/logomeio.png","imagem/playBlackjack.png","imagem/playMegastacker.png","imagem/playSlotmachine.png","imagem/qtdcarteira.png","imagem/triangulodireita2.png","imagem/trinagulodireita.png","imagem/voltar2.png","imagem/icone2.png"],
+        include_msvcr = True,
         excludes = []
 )
 
@@ -38,9 +42,9 @@ buildOptions = dict(
 
 
 setup(
-    name = "GNUcasino",
+    name = "GNUScasinos",
     version = "1.0",
     description = "Simulador de casinos",
     options = dict(build_exe = buildOptions),
-    executables = executables
+    executables = executable
  )
